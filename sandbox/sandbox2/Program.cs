d6fda7d80cb3305a9baba820942fc5ca51013ae8
Jens <jensplaysbass@gmail.com>
2024-09-25 12:51:00 -0600

    updated sandbox2

--- sandbox/sandbox2/Program.cs
+++ sandbox/sandbox2/Program.cs
@@ -4,8 +4,15 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Console.WriteLine("Hello, World! ");
+        Console.WriteLine("test");
+        Console.WriteLine("test 2");
+        string jd;
+        jd = "this is str jd";
+        Console.WriteLine("jd variable: ", jd);
+        for(int i = 1; i<=10; i++)
+        {
+            Console.WriteLine(i);
+        }
     }
 }
-
-
