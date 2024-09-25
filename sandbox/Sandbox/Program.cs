824047c51917766143e5e0bc2b120b21fa4c9dc6
Jens <jensplaysbass@gmail.com>
2024-09-25 13:41:05 -0600

    sandbox 2

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -4,11 +4,23 @@ class Program
 {
     static void Main(string[] args)
     {
-        List<int> x;
-        x = [24, 25, 26];
+        List<double> x = new();
+
+        double input;
+        input = 1;
+        while(input != 0)
+        {
+            Console.Write("Enter something: ");
+            string input_str = Console.ReadLine();
+            input = double.Parse(input_str);
+            x.Add(input);
+        }
+        Console.WriteLine("Entered Values:");
+        int value = 1;
         foreach(int i in x)
         {
-            Console.WriteLine(i);
+            Console.WriteLine($"Value {value}: {i}");
+            value++;
         }
     }
 }
